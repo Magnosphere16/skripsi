7f7601b8e0fc242b9f961ac38a848a97f901bbb9
Michael <michaelljonathan16@gmail.com>
2022-01-26 21:59:04 +0700

    KassaKu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>StockInFlow</title>
+    <title>KassaKu</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
